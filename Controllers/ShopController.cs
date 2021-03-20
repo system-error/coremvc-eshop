927d12a5b5eaddd5307a370a1bd5dab9daa140a2
gprok <gprok60@gmail.com>
2021-03-20 19:28:18 +0200

    shop catalog page

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using roles.Data;
+using roles.Models;
+
+namespace eshop.Controllers
+{
+    public class ShopController : Controller
+    {
+        ApplicationDbContext _db;
+
+        public ShopController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _db.Products.Include(c => c.Category).ToList();
+            ViewBag.products = products;
+            return View();
+        }
+    }
+}
