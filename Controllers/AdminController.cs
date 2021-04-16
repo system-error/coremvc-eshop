3a56e3e90fcede51fd0d73c5e8fac2ac2d94f246
XRISTOS <christos_dousmanopoulos@yahoo.gr>
2021-04-16 22:13:52 +0300

    added the "add product" page.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -70,5 +70,56 @@ namespace eshop.Controllers
             _db.SaveChanges();
             return RedirectToAction("Products", "Admin");
         }
+
+        
+        public IActionResult AddProduct()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            Product product = new Product();
+            ViewBag.product = product;
+            ViewBag.categories = categories;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(string productName, string description, string productPrice, int productStock, IFormFile productImage,int productCategory)
+        {
+        
+            string path = Path.Combine(this._environment.WebRootPath, "uploads/images");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        
+            Product product = new Product();
+            product.Name = productName;
+            product.Description = description;
+            product.Stock = productStock;
+            
+            product.Category = _db.Categories.Find(productCategory);
+            product.Price = decimal.Parse(productPrice.Replace(".", ","));
+            if (productImage != null)
+            {
+        
+                string fileName = Path.GetFileName(productImage.FileName);
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    productImage.CopyTo(stream);
+                    product.Image = fileName;
+                }
+            }
+            else
+            {
+                product.Image = "";
+            }
+            //TODO find the previous Id from the database and Add +1
+            //Maybe work without add if I have the id
+            _db.Add(product);
+            _db.SaveChanges();
+            return RedirectToAction("Products", "Admin");
+        }
+
+
     }
 }
